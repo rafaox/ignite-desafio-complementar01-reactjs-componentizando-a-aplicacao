f027c20aabfa4ed5f845875ea5804dfdc9a19424
Rafael Cardoso <rflcardoso90@gmail.com>
2021-04-19 21:33:51 -0300

    resolução do desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,28 @@
+import { useWatchMe } from "../hooks/useWatchMe";
+import { MovieCard } from "./MovieCard";
+
 export function Content() {
-  // Complete aqui
+  const { selectedGenre, movies } = useWatchMe();
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard
+              key ={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
